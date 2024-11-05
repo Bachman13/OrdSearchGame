d6f07f73924b9c88cbea9d8ed59074bd8dbcad6c
Bachman13 <andre.bachman@brightstraining.com>
2024-11-05 14:08:05 +0100

    Sen sparning

--- OrdSearchGame.ConsoleApp/OrdSearchUI.cs
+++ OrdSearchGame.ConsoleApp/OrdSearchUI.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OrdSearchGame.ConsoleApp;
+using OrdSearchGame.Core;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Word word = new Word();
+
+        var wordList = word.ReadWordsFromFile();
+        var selectedWords = word.GenerateRandomWords(wordList);
+
+        foreach(var words in selectedWords)
+        {
+            System.Console.WriteLine(words);
+        }
+
+
+
+
+    }
+}
