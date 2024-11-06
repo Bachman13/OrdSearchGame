a6b438196ad63cecb8fe99340742ef26bd41c4f6
Bachman13 <andre.bachman@brightstraining.com>
2024-11-06 16:17:15 +0100

    Test klart

--- OrdSearchGame.Tests/OrdSearchTest.cs
+++ OrdSearchGame.Tests/OrdSearchTest.cs
@@ -1,10 +1,17 @@
+using Shouldly;
+
 namespace OrdSearchGame.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void ReturnFiveWords()
     {
+        Word word1 = new Word();
+        List<string> wordList = new List<string>{"APPLE", "NIGHT", "STONE", "KING", "QUEEN"};
 
+        var selectedWordList = word1.GenerateRandomWords(wordList);
+
+        selectedWordList.Count.ShouldBe(5);
     }
 }
